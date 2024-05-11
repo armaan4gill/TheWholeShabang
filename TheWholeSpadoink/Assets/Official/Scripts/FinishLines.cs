2dcd5f59c8081b80e49dfb2c5053dc111db9a3d5
armaan4gill <armaan4gill@gmail.com>
2024-05-10 22:21:59 -0700

    Finsih lines

---
+++ TheWholeSpadoink/Assets/Official/Scripts/FinishLines.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLines : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.gameObject.tag == "Player")
+        {
+            // Load the next scene by name 
+            SceneManager.LoadScene(6);
+        }
+    }
+}
